922c3048d745cb39f919396dba752b259572b040
Om Vyawahare <YOUR_GITHUB_EMAIL>
2026-08-12 15:46:33 +0530

    Added python homework files

---
+++ aug4.py
@@ -0,0 +1,9 @@
+age =19
+
+city = "pune"
+
+# print("age, city) crt + /
+print(type(city))
+
+print(id(age))
+print(id(city))
---
+++ task1 identity card .py
@@ -0,0 +1,8 @@
+print("my id card")
+print("name: om")
+print("age: 23")
+print("city: akola")
+print("dob: 02/10/2003")
+print("collage: skc")
+print("blood group:A+")
+
